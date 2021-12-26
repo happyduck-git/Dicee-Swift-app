6952aaa72b689eb8f8aa028877def95be396fd98
GG <rkrudtls@gmail.com>
2021-12-26 23:37:13 +0900

    Complete Dicee app

---
+++ Dicee-Swift/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Dicee-Swift
+//
+//  Created by HappyDuck on 2021/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack{
+                    DiceView(number: leftDiceNumber)
+                    DiceView(number: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+           
+
+    }
+}
+
+struct DiceView: View {
+    let number: Int
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Dicee-Swift/Dicee_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftApp.swift
+//  Dicee-Swift
+//
+//  Created by HappyDuck on 2021/09/27.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
